5e7b26649e4c07f47fe8ba92eca2a3d2220aeb6b
donny <donnyrizaladhip@rocketmail.coom>
2026-01-03 21:53:22 +0700

    Finale

--- Module/script.js
+++ Module/script.js
@@ -45,10 +45,10 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function toSebLink(url) {
-        if (!url) return "#";
-        const absoluteUrl = new URL(url, window.location.href).href;
-        return absoluteUrl.replace(/^https?:/, 'sebs:');
-    }
+    if (!url) return "#";
+    const absoluteUrl = new URL(url, window.location.href).href;
+    return absoluteUrl.replace(/^https?:/, "sebs:");
+  }
 
   function updateTime() {
     const now = new Date();
@@ -147,9 +147,9 @@ document.addEventListener("DOMContentLoaded", () => {
                             <a class="seb" href="${toSebLink(course.link)}">${
               course.name
             }</a>
-                            <a href="${toSebLink(
+                            <a href="${
                               course.link
-                            )}" class="inline-flex items-center justify-center p-1 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+                            }" class="inline-flex items-center justify-center p-1 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
                                             <svg class="w-4 h-4 rtl:rotate-180" aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                                 <path stroke="currentColor" stroke-linecap="round"
